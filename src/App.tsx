1a5a088b9f195e301381d38427be1666db2d197b
tallboy <yourspaceworkteam@gmail.com>
2022-05-27 14:05:49 +0300

    edit branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+import Table from './components/Table/Table'
+import users from "./components/Table/users.json";
+import posts from "./components/Table/posts.json";
+import comments from "./components/Table/todos.json";
+import {IUser} from './components/Table/Types'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //Data for tables headers and key json
+    const usersColumns = React.useMemo(
+        () => [
+            {
+                Header: "Id",
+                accessor: "id"
+            },
+            {
+                Header: "Name",
+                accessor: "name"
+            },
+            {
+                Header: "User Name",
+                accessor: "username"
+            },
+        ],
+        []
+    );
+
+    const postsColumns = React.useMemo(
+        () => [
+            {
+                Header: "Id",
+                accessor: "id"
+            },
+            {
+                Header: "Title",
+                accessor: "title"
+            },
+            {
+                Header: "Body",
+                accessor: "body"
+            }
+        ],
+        []
+    );
+
+    const postsComments = React.useMemo(
+        () => [
+            {
+                Header: "Id",
+                accessor: "id"
+            },
+            {
+                Header: "Name",
+                accessor: "name"
+            },
+            {
+                Header: "Email",
+                accessor: "email"
+            }
+        ],
+        []
+    );
+
+    // Data for tables rows
+    const dataUsers = React.useMemo(() => users, []);
+    const dataPosts = React.useMemo(() => posts, []);
+    const dataComments = React.useMemo(() => comments, []);
+
+    return (
+        <div className='App'>
+            <Table columns={usersColumns} data={dataUsers}/>
+            <Table columns={postsColumns} data={dataPosts}/>
+            <Table columns={postsComments} data={dataComments}/>
+        </div>
+
+    );
 }
 
 export default App;
